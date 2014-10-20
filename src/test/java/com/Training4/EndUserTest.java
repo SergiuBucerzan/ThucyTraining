0bff9b74defdff354d353fcfa7731668479282d7
danielagalea <daniela.galea@evozon.com>
2014-10-20 18:11:41 +0300

    steps for my request find content "Filter requests"

--- src/test/java/com/Training4/EndUserTest.java
+++ src/test/java/com/Training4/EndUserTest.java
@@ -1,6 +1,7 @@
 package com.Training4;
 
 import net.thucydides.core.annotations.Managed;
+
 import com.Training4.tools.Constants; 
 
 import net.thucydides.core.annotations.ManagedPages;
@@ -9,12 +10,14 @@ import net.thucydides.core.annotations.Story;
 import net.thucydides.core.pages.Pages;
 import net.thucydides.junit.runners.ThucydidesRunner;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
 import com.Training4.requirements.Application;
 import com.Training4.steps.LoginSteps;
+import com.Training4.steps.MyRequestSteps;
 
 @Story(Application.Search.SearchByKeyword.class)
 @RunWith(ThucydidesRunner.class)
@@ -29,9 +32,16 @@ public class EndUserTest {
     @Steps
     LoginSteps loginSteps;
     
+    @Steps
+    MyRequestSteps myRequestSteps;
+    
     @Test
     public void create_vacation(){
     	loginSteps.login_and_go_to_vacation(Constants.END_USER, Constants.END_PASS);
+    	myRequestSteps.go_to_MyRequestsPage();
+    	
+    	//Assert.assertEquals("Content title is not as expected!!!", myRequestSteps.get_content_title().contentEquals("Filters requests"));
+    	Assert.assertEquals("Filter requests", myRequestSteps.get_content_title());
     	
     }
  
