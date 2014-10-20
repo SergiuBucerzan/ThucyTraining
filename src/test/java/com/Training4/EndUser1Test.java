731e8fe78c8e288e418a6a1ababbbf83bb366dd1
ralucamoroianu <ralucamoroianu@EVO361.evointern>
2014-10-20 16:39:48 +0300

    added EndUser1Test

---
+++ src/test/java/com/Training4/EndUser1Test.java
@@ -0,0 +1,38 @@
+package com.Training4;
+
+import net.thucydides.core.annotations.Managed;
+import com.Training4.tools.Constants; 
+
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.Training4.requirements.Application;
+import com.Training4.steps.LoginSteps;
+
+@Story(Application.Search.SearchByKeyword.class)
+@RunWith(ThucydidesRunner.class)
+public class EndUser1Test {
+    
+	@Managed(uniqueSession = true)
+    public WebDriver webdriver;
+    
+    @ManagedPages(defaultUrl = Constants.BASE_URL)
+    public Pages pages;
+    
+    @Steps
+    LoginSteps loginSteps;
+    
+    @Test
+    public void create_vacation(){
+    	loginSteps.login_and_go_to_vacation(Constants.END_USER1, Constants.END_PASS1);
+    	
+    }
+ 
+} 
