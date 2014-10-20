699dd7fd11d35c4b77e66712188a93d96f355895
sergiubucerzan <sergiubucerzan@EVO030.evointern>
2014-10-20 15:45:28 +0300

    updates on login test

--- src/test/java/com/Training4/LoginTest.java
+++ src/test/java/com/Training4/LoginTest.java
@@ -30,27 +30,9 @@ public class LoginTest {
     LoginSteps loginSteps;
     
     @Test
-    public void login_test(){
+    public void successfull_login_test(){
     	loginSteps.login(Constants.DM_USER, Constants.DM_PASS);
+    	
     }
  
-//    @Steps
-//    public LoginSteps endUser;
-//
-//    @Issue("#WIKI-1")
-//    @Test
-//    public void searching_by_keyword_apple_should_display_the_corresponding_article() {
-//        endUser.is_the_home_page();
-//		endUser.looks_for("apple");
-//        endUser.should_see_definition("A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.");
-//
-//    }
-//
-//    @Test
-//    public void searching_by_keyword_banana_should_display_the_corresponding_article() {
-//        endUser.is_the_home_page();
-//		endUser.looks_for("pear");
-//		endUser.should_see_definition("An edible fruit produced by the pear tree, similar to an apple but elongated towards the stem.");
-//    }
-
 } 
