9bbcb5c7a8cb43baa68e01e5e60b9b22c6853ff7
mihaiarsin <mihai.arsin@evozon.com>
2014-10-21 10:55:07 +0300

    Added Vacation Management View Vacations stuff

---
+++ src/test/java/com/Training4/steps/VacationManagementViewVacationPageSteps.java
@@ -0,0 +1,42 @@
+package com.Training4.steps;
+
+import net.thucydides.core.annotations.Managed;
+
+import com.Training4.tools.Constants; 
+
+import net.thucydides.core.annotations.ManagedPages;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.pages.Pages;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.Training4.pages.VacationManagementIndexPage;
+import com.Training4.pages.VacationManagementViewVacationsPage;
+import com.Training4.requirements.Application;
+import com.Training4.steps.LoginSteps;
+
+@Story(Application.Search.SearchByKeyword.class)
+@RunWith(ThucydidesRunner.class)
+public class VacationManagementViewVacationPageSteps {
+    
+	@Managed(uniqueSession = true)
+    public WebDriver webdriver;
+    
+    @ManagedPages(defaultUrl = Constants.BASE_URL)
+    public Pages pages;
+
+    LoginSteps loginSteps;
+
+    VacationManagementViewVacationsPage viewVacationItem;
+    @Step
+	public void clickViewVacationItem() {
+    	viewVacationItem.clickViewVacationItem();
+		
+	}
+   
+}
